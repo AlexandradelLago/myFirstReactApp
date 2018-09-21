b3977b4830c06f0498d34b74b33c6cd612c4a12c
alexandradelLago <daviniaroca@gmail.com>
2018-09-22 00:12:25 +0200

    to do list- tutorial 1

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './List';
 import MyComponent from './MyComponent';
+
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isOn: true
+        term:'hola que tal',
+        items:[]
     };
   }
-  toggle = () => {
-    this.setState({isOn: !this.state.isOn})
+  onChange = (event) => {
+    this.setState({term: event.target.value});
+  };
+  whenSending = (event)=>{
+    event.preventDefault();
+    this.setState({
+      term:'',
+      items:[...this.state.items,this.state.term]
+    })
   }
-
-
-
-
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Daviniás first app</h1>
-          <MyComponent name="davinia"/>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick={this.toggle}>
-          {this.state.isOn ? 'On' : 'Off'}
-        </button>
+      <div>
+          <form className="App" onSubmit={this.whenSending}>
+          <input value={this.state.term} onChange={this.onChange} />
+          <button>Submit</button>
+          </form>
+          <MyComponent></MyComponent>
+          <List items={this.state.items} />
       </div>
     );
   }
