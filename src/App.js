9fce293dfba779c81f4b41a57616350b5ffa1d2e
alexandradelLago <daviniaroca@gmail.com>
2018-09-22 10:55:39 +0200

    tutorial 2 increment decrement

--- src/App.js
+++ src/App.js
@@ -9,10 +9,21 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      // intro react
+      isOn:true,
+      // todo list
         term:'hola que tal',
-        items:[]
+        items:[],
+        // incremente decrement exercise
+        clicks:0,
+        show:true
     };
   }
+  // intro react
+  toggle = () => {
+    this.setState({isOn: !this.state.isOn})
+  }
+  // to do list
   onChange = (event) => {
     this.setState({term: event.target.value});
   };
@@ -23,15 +34,34 @@ class App extends Component {
       items:[...this.state.items,this.state.term]
     })
   }
+  // increment decrement
+ IncrementItem =()=>{
+   this.setState({clicks:this.state.clicks+1});
+ };
+ DecrementItem=()=>{
+  this.setState({clicks:this.state.clicks-1});
+}
+ToggleClick=()=>{
+  this.setState({show:!this.state.show});
+}
+
+// todo mi html , la vista
   render() {
     return (
       <div>
+          <p>To Do list </p>
           <form className="App" onSubmit={this.whenSending}>
           <input value={this.state.term} onChange={this.onChange} />
           <button>Submit</button>
           </form>
-          <MyComponent></MyComponent>
           <List items={this.state.items} />
+          <p>Intro React </p>
+          <button onClick={this.toggle}> {this.state.isOn ? 'On' : 'Off'}</button>
+          <MyComponent name={"Davinia :)"}></MyComponent>
+          <button onClick={this.IncrementItem}> +</button>
+          <button onClick={this.DecrementItem}> -</button>
+          <button onClick={this.ToggleClick}> {this.state.show? "Hide number":"Show Number"}</button>
+          {this.state.show? <h2>{this.state.clicks}</h2>: ""}
       </div>
     );
   }
