4e26dd94ac9450f9e9288b52f99d6b917cccec3f
alexandradelLago <daviniaroca@gmail.com>
2018-09-23 15:41:17 +0200

    first react testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// added to test with tutorial 6
+import renderer from 'react-test-renderer';
+import toJson from 'enzyme-to-json';
+import { shallow } from 'enzyme';
+
+
+
+describe('Component: App', () => {
+  const items = ['Learn react', 'rest', 'go out'];
+
+  it('should match its empty snapshot', () => {
+    const tree = renderer.create(
+      <App />
+     ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
